f889f74bfb63ff0d1af709ea184529c34785a8a1
anabiz <anabizconcept9@gmail.com>
2020-11-01 13:57:41 +0100

    update

--- src/stores/pg.ts
+++ src/stores/pg.ts
@@ -2,6 +2,4 @@ import postgres from "postgres";
 import dotenv from "dotenv";
 
 dotenv.config();
-export const sql = postgres(
-  	'postgres://deqhvskx:SvzignKZBh0q1jz-vVBcwpHh5_sFmkRq@kandula.db.elephantsql.com:5432/deqhvskx'
-);
+export const sql = postgres(`${process.env.DATABASE_URL}`);

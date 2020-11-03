5cf932d46f8ea4e9deb6414acf372ef0fa29e4fe
anabiz <anabizconcept9@gmail.com>
2020-11-03 14:54:22 +0100

    update

--- src/emailBodyMessages/verifyCompanyEmailBody.ts
+++ src/emailBodyMessages/verifyCompanyEmailBody.ts
@@ -1,4 +1,4 @@
 export const companyVerifyEmailBody = (
   token: string,
 ) => `<h3>Please, click on the link bellow to verify your company email</h3>
-                                             <P><a href='http://localhost:3000/apiv1/company/verifycompanyemail/${token}' rel='noopener noreferrer'>click here to verify your email</a> </P> `;
+                                             <P><a href='https://datamanager1.herokuapp.com/apiv1/company/verifycompanyemail/${token}' rel='noopener noreferrer'>click here to verify your email</a> </P> `;
